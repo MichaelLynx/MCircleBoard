c3bd7f6dc32eebbb33053cb2e331f434097de023
厦门巨烨科技 <service@zayata.com>
2019-01-14 17:26:48 +0800

    Initial Commit

---
+++ CircleViewDownDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleViewDownDemo
+//
+//  Created by Zayata on 2019/1/14.
+//  Copyright © 2019年 Zayata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
