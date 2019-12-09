066b02b95af15eaf8b5574540494adde0cf35e22
whitesage <1216831710@qq.com>
2019-12-09 18:09:55 +0800

    去除第三方

--- MCircleBoard/ViewController.swift
+++ MCircleBoard/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SnapKit
 
 let WIDTH_SCREEN = Double(UIScreen.main.bounds.size.width)
 let HEIGHT_SCREEN = Double(UIScreen.main.bounds.size.height)
@@ -51,43 +50,16 @@ class ViewController: UIViewController {
         
         let array4:[MCircleItemType] = [.white, .white, .white, .white, .white, .yellow, .yellow, .yellow, .yellow, .yellow, .black, .black, .black, .black, .red, .red, .red]
         
-        let circleBoard1 = MCircleBoard(array: array1)
-        circleBoard1.frame = CGRect(x: 10, y: 60, width: 150, height: 150)
-        view.addSubview(circleBoard1)
-        
-        let circleBoard2 = MCircleBoard()
-        circleBoard2.setInterface(defaultArray: array2)
-        view.addSubview(circleBoard2)
-        
-        let circleBoard3 = MCircleBoard(array: array3)
-        view.addSubview(circleBoard3)
-        
-        let circleBoard4 = MCircleBoard(defaultArray: array4, isClockWise: false)
-        view.addSubview(circleBoard4)
-        circleBoard4.backgroundColor = .orange
+        let circleBoard = MCircleBoard(array: array1)
+        circleBoard.frame = CGRect(x: 100, y: 60, width: 200, height: 200)
+        view.addSubview(circleBoard)
         
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-            circleBoard1.setInterface(defaultArray: [(.green, 12),(.yellow, 5), (.black, 10) ,(.red, 13)])
-            circleBoard3.setInterface(array: array3, isClockWise: false)
+            circleBoard.setInterface(defaultArray: array2)
+            //circleBoard.setInterface(array: array3)
+            //circleBoard.setInterface(defaultArray: array4)
         }
         
-        circleBoard2.snp_makeConstraints { (make) in
-            make.top.equalTo(60)
-            make.width.height.equalTo(200)
-            make.right.equalTo(view).offset(-20)
-        }
-        
-        circleBoard3.snp_makeConstraints { (make) in
-            make.height.width.equalTo(200)
-            make.top.equalTo(circleBoard2.snp_bottom).offset(50)
-            make.centerX.equalToSuperview()
-        }
-        
-        circleBoard4.snp_makeConstraints { (make) in
-            make.height.width.equalTo(200)
-            make.top.equalTo(circleBoard3.snp_bottom).offset(20)
-            make.right.equalToSuperview().offset(-50)
-        }
     }
 }
 
--- Pods/Target Support Files/Pods-MCircleBoard/Pods-MCircleBoard-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MCircleBoard : NSObject
-@end
-@implementation PodsDummy_Pods_MCircleBoard
-@end
